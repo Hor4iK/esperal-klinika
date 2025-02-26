3d7c3806efe466790d8b34ce6785839ba6c3b99f
VladimirPuzanov <volodya938@yandex.ru>
2025-02-26 21:20:08 +0300

    Всё ещё верстаем шапку

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -1,2 +1,2 @@
-let menuAccordionButtons=document.querySelectorAll(".menu__sub-open");menuAccordionButtons.forEach(e=>{e.addEventListener("click",()=>{e.classList.toggle("active")})});
+let menuAccordionButtons=document.querySelectorAll(".menu__sub-open");menuAccordionButtons.forEach(e=>{e.addEventListener("click",()=>{e.classList.toggle("active"),e.closest("li").classList.toggle("active")})});
 //# sourceMappingURL=main.js.map
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,6 +1,9 @@
+//Открывашка аккордеонов в шапке мобильной версии
 const menuAccordionButtons = document.querySelectorAll('.menu__sub-open');
 menuAccordionButtons.forEach(accordionButton => {
 	accordionButton.addEventListener("click", () => {
 		accordionButton.classList.toggle("active");
+		const menuElement = accordionButton.closest("li");
+		menuElement.classList.toggle("active");
 	})
 });
