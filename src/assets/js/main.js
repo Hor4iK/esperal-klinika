1e83242bb0d90d9a0ce371010f9eba8530ca1aba
VladimirPuzanov <volodya938@yandex.ru>
2025-03-09 22:47:04 +0300

    Сверстал конверсионную карточку, начал верстать первый экран главной старницы

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -14,11 +14,29 @@ if (menuAccordionButtons) {
 
 //Открывашка бургера в мобильной версии
 const burger = document.querySelector(".ham")
-if(burger){
-	burger.addEventListener("click", ()=>{
+if (burger) {
+	burger.addEventListener("click", () => {
 		const header = document.querySelector(".header");
-		if(header){
+		if (header) {
 			header.classList.toggle("active");
 		}
+	})
+}
+
+//Плашка снизу сайта
+let converseCardWrapper = document.querySelector(".converse__card-wrapper");
+if (converseCardWrapper) {
+	let converseClose = document.querySelector(".converse__close");
+	//let getUpButton = document.querySelector("#toTop");
+	//getUpButton.style.bottom = "160px";
+	//getUpButton.style.transition = "0.3s";
+	converseClose.addEventListener("click", () => {
+		// if (getUpButton.style.bottom == "160px") {
+		// 	getUpButton.style.bottom = "45px";
+		// } else {
+		// 	getUpButton.style.bottom = "160px";
+		// }
+		converseClose.classList.toggle("converse__close_hidden");
+		converseCardWrapper.classList.toggle("converse__card-wrapper_hidden");
 	})
 }
