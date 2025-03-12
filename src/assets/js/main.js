2cec08c7fd95d71c589f38acbc8af2c7fe6737ca
VladimirPuzanov <volodya938@yandex.ru>
2025-03-12 15:32:02 +0300

    Верстал, верстал, устал верстать, сейчас поеду домой. Делал 2 часа чёртов слайдер при ресайзе

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -41,7 +41,56 @@ if (converseCardWrapper) {
 	})
 }
 
-Fancybox.bind("[data-fancybox]");
+//Табы для услуг
+const serviceTabsBtns = document.querySelectorAll(".our-services__tabs-list li")
+const serviceTabContents = document.querySelectorAll(".our-services__tab-content");
+if (serviceTabsBtns && serviceTabContents) {
+	if (window.innerWidth > 960) {
+		serviceTabsBtns[0].classList.add("active");
+		serviceTabContents[0].classList.add("active");
+	}
+	for (let i = 0; i < serviceTabContents.length; i++) {
+		serviceTabContents[i].style.order = i * 2 + 1;
+		serviceTabsBtns[i].style.order = i * 2;
+		serviceTabsBtns[i].addEventListener('click', () => {
+			if (window.innerWidth > 960) {
+				document.querySelectorAll(".our-services__tabs-list li.active").forEach(activeBtn => {
+					activeBtn.classList.remove('active');
+				})
+				document.querySelectorAll(".our-services__tab-content.active").forEach(activeContent => {
+					activeContent.classList.remove('active');
+				})
+				serviceTabsBtns[i].classList.add('active');
+				serviceTabContents[i].classList.add("active");
+			}
+			else {
+				serviceTabsBtns[i].classList.toggle('active');
+				serviceTabContents[i].classList.toggle("active");
+				if (serviceTabContents[i].style.maxHeight) {
+					serviceTabContents[i].style.maxHeight = ""
+				} else {
+					serviceTabContents[i].style.maxHeight = serviceTabContents[i].scrollHeight + 40 + "px";
+				}
+			}
+		})
+	}
+}
+
+//Баян faq
+const questions = document.querySelectorAll('.faq__question-wrapper');
+const answers = document.querySelectorAll(".faq__answer");
+if (questions && answers) {
+	for (let i = 0; i < questions.length; i++) {
+		questions[i].addEventListener("click", () => {
+			questions[i].classList.toggle("active");
+			if (answers[i].style.maxHeight) {
+				answers[i].removeAttribute("style");
+			} else {
+				answers[i].style.maxHeight = document.querySelector(".faq__answer").scrollHeight + "px";
+			}
+		})
+	};
+}
 
 const trustSliderCheck = document.querySelectorAll('.trust');
 if (trustSliderCheck.length > 0) {
@@ -52,11 +101,11 @@ if (trustSliderCheck.length > 0) {
 			grabCursor: true,
 			spaceBetween: 10,
 			navigation: {
-				nextEl: slider.querySelector('nextButton'),
-				prevEl: slider.querySelector('prevButton'),
+				nextEl: slider.querySelector('.trust__nav-btn_next'),
+				prevEl: slider.querySelector('.trust__nav-btn_prev'),
 			},
 			breakpoints: {
-				360: {
+				0: {
 					slidesPerView: 1,
 					spaceBetween: 20,
 				},
@@ -68,3 +117,32 @@ if (trustSliderCheck.length > 0) {
 		});
 	})
 }
+
+//Слайдер, который вырубается при ресайзе
+advantagesSwiperCheck = document.querySelector(".advantages__swiper");
+if (advantagesSwiperCheck) {
+	advantagesSwiper = false;
+	['resize', 'load'].forEach((event) => {
+		window.addEventListener(event, function () {
+			if (window.innerWidth < 650 && !advantagesSwiper) {
+				advantagesSwiper = new Swiper(document.querySelector('.advantages__swiper'), {
+					direction: 'horizontal',
+					slidesPerView: 1,
+					grabCursor: true,
+					spaceBetween: 10,
+					navigation: {
+						nextEl: document.querySelector('.advantages__nav-btn_next'),
+						prevEl: document.querySelector('.advantages__nav-btn_prev'),
+					},
+				});
+			}
+			if (window.innerWidth >= 650 && advantagesSwiper){
+				advantagesSwiper.destroy(true, true);
+				advantagesSwiper = false
+			}
+		})
+	})
+}
+
+//Просмотр для фотографий
+Fancybox.bind("[data-fancybox]");
