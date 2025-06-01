92e6b2926fe738b0ac8e4cb2f7b11b0b127f1ff6
Hor4iK <kiasdog@mail.ru>
2025-06-01 18:50:59 +0300

    end Intro-service (adaptive/swiper) && add breadcrumbs

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -11,3 +11,38 @@ if (searchArray) {
     })
   })
 }
+
+//Slider, turns off when resize
+introSwiper = document.querySelector(".intro__swiper");
+if (introSwiper) {
+	introSwiperCheck = false;
+	['resize', 'load'].forEach((event) => {
+		window.addEventListener(event, function () {
+			if (window.innerWidth <= 650 && !introSwiperCheck) {
+				introSwiperCheck = new Swiper(document.querySelector('.intro__swiper'), {
+					direction: 'horizontal',
+					slidesPerView: 1,
+					grabCursor: true,
+					spaceBetween: 10,
+					navigation: {
+						nextEl: document.querySelector('.intro__nav-btn_next'),
+						prevEl: document.querySelector('.intro__nav-btn_prev'),
+					},
+          breakpoints: {
+            0: {
+              slidesPerView: 1.1,
+              spaceBetween: 10
+            }
+          }
+				});
+			}
+			if (window.innerWidth > 650 && introSwiperCheck) {
+				introSwiperCheck.destroy(true, true);
+				introSwiperCheck = false
+			}
+		})
+	})
+}
+
+//view photos fancybox
+Fancybox.bind("[data-fancybox]");
