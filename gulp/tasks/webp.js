760d195506b0a157159d21072b1fcf02923317ef
Hor4iK <142799083+Hor4iK@users.noreply.github.com>
2025-06-10 23:15:53 +0300

    Delete gulp directory

--- gulp/tasks/webp.js
+++
@@ -1,8 +0,0 @@
-import webp from "gulp-webp";
-
-export function webpRun() {
-  return app.gulp
-    .src(`${app.path.src.imagesWebp}`, { encoding: false })
-    .pipe(webp())
-    .pipe(app.gulp.dest(app.path.dist.images));
-}
