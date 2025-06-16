3bce7d6bf4c478a2ce353bfb324c667aa49b4dfa
Hor4iK <kiasdog@mail.ru>
2025-06-16 20:55:43 +0300

    optimization attempt && animations && fixed bugs

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -964,6 +964,35 @@ document.addEventListener('DOMContentLoaded', function () {
   /* -- END TOTOP -- */
 
 
+  const observer = new IntersectionObserver((entries) => {
+  let animationDelay = 0;
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      if (entry.target.classList.contains("animation-group")) {
+        animationDelay = animationDelay + 100
+      } else {
+        animationDelay = 0;
+      }
+      setTimeout(() => {
+        entry.target.classList.add("animated")
+      }, animationDelay);
+      observer.unobserve(entry.target)
+    }
+  });
+}, {
+  threshold: 0.3,
+  rootMargin: '50px',
+});
+
+const animatedItems = document.querySelectorAll(".to_animate")
+
+if (animatedItems.length > 0) {
+  animatedItems.forEach(item => {
+    observer.observe(item)
+  })
+}
+
+
 
   //view photos fancybox
   Fancybox.bind("[data-fancybox]");
